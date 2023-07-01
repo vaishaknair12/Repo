dc5c7817d341debf47dc6e2b32e01825a8530cc8
vaish <pradeepvaishak3000@gmail.com>
2023-07-01 18:07:45 +0530

    cahnges

--- server.js
+++ server.js
@@ -68,6 +68,10 @@ app.post('/api/signin', async (req, res) => {
 	res.json({ status: 'Failure', error: 'Invalid email/password' })
 })
 
+app.get('/', function(req, res){
+    res.render('index.ejs');
+  });
+
 app.listen(3000 , () =>{
     console.log("server listened")
 })
